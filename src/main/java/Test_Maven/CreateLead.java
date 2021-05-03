7e804c07383b1bca2e45cecc0cc0e9a05be07598
livin1993 <llivingston007@gmail.com>
2021-05-03 13:50:04 +0530

    gradle commit

---
+++ src/main/java/Test_Maven/CreateLead.java
@@ -0,0 +1,33 @@
+package Test_Maven;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+
+public class CreateLead {
+	
+	@Test
+	public void createLeadTestCase() {
+	
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://leaftaps.com/opentaps/");
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		driver.findElementById("username").sendKeys("DemoSalesManager");
+		driver.findElementById("password").sendKeys("crmsfa");
+		driver.findElementByClassName("decorativeSubmit").click();
+		driver.findElementByLinkText("CRM/SFA").click();
+		driver.findElementByLinkText("Leads").click();
+		driver.findElementByLinkText("Create Lead").click();
+		driver.findElementById("createLeadForm_companyName").sendKeys("TestLeaf");
+		driver.findElementById("createLeadForm_firstName").sendKeys("Gopi");
+		driver.findElementById("createLeadForm_lastName").sendKeys("J");
+		driver.findElementByName("submitButton").click();
+		driver.close();
+	}
+}
